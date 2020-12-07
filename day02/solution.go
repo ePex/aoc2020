0d17d43a62f4e0ccbb1dbd4655daf57e408b5721
Philipp Holz <phil.epex@googlemail.com>
2020-12-08 00:52:57 +0100

    refactoring of extraction method

--- day02/solution.go
+++ day02/solution.go
@@ -9,20 +9,26 @@ func CalculateNumberOfValidPasswordsSledPolicy(passwordData []string) (count int
 	result := 0
 
 	for _, rulePasswordPair := range passwordData {
-		rule := strings.Split(rulePasswordPair, ":")[0] // "1-3 a: abcde", "1-3 b: cdefg", "2-9 c: ccccccccc"
+		min, max, char, password := extractPasswordAndRule(rulePasswordPair)
 
-		expectedCount := strings.Split(rule, " ")[0]
-		least, _ := strconv.Atoi(strings.Split(expectedCount, "-")[0])
-		most, _ := strconv.Atoi(strings.Split(expectedCount, "-")[1])
-
-		char := strings.Split(rule, " ")[1]
-		password := strings.Split(rulePasswordPair, ":")[1]
 		count := strings.Count(password, char)
-
-		if count >= least && count <= most {
+		if count >= min && count <= max {
 			result++
 		}
 	}
 
 	return result
+}
+
+func extractPasswordAndRule(policyWithPassword string) (min int, max int, char string, password string) {
+	rule := strings.Split(policyWithPassword, ":")[0] // "1-3 a: abcde", "1-3 b: cdefg", "2-9 c: ccccccccc"
+
+	expectedCount := strings.Split(rule, " ")[0]
+	min, _ = strconv.Atoi(strings.Split(expectedCount, "-")[0])
+	max, _ = strconv.Atoi(strings.Split(expectedCount, "-")[1])
+
+	char = strings.Split(rule, " ")[1]
+	password = strings.Split(policyWithPassword, ":")[1]
+
+	return min, max, char, password
 }
