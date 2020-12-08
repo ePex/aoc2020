f2ef72ce198a80873c4532f87a6903336c0b26dc
Philipp Holz <phil.epex@googlemail.com>
2020-12-08 22:12:45 +0100

    setup day04 to 08

---
+++ day05/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	utils "aoc2020/util"
+	"fmt"
+)
+
+func main() {
+	values := utils.FileString("day05/input.txt")
+
+	fmt.Printf("Solution part one: %d\n", values)
+	fmt.Printf("Solution part two: %d\n", values)
+}
