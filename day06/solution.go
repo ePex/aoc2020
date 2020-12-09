56ef70902c51ffd3c02f4b72579cae3b20890927
Philipp Holz <phil.epex@googlemail.com>
2020-12-09 23:55:52 +0100

    day 6 solution 1

--- day06/solution.go
+++ day06/solution.go
@@ -1,1 +1,25 @@
 package main
+
+func CalculateYesCount(batch []string) (answerCount int) {
+	var groupAnswers []string
+	for idx, answers := range batch {
+		groupAnswers = append(groupAnswers, answers)
+		if answers == "" || idx+1 == len(batch) {
+			answerCount += countGroup(groupAnswers)
+			groupAnswers = []string{}
+		}
+	}
+
+	return answerCount
+}
+
+func countGroup(groupAnswers []string) int {
+	answerMap := make(map[string]int)
+	for _, personAnswers := range groupAnswers {
+		for _, answer := range personAnswers {
+			answerMap[string(answer)] = 1
+		}
+	}
+
+	return len(answerMap)
+}
--- day06/solution_test.go
+++ day06/solution_test.go
@@ -1,1 +1,52 @@
 package main
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestWithOneGroup(t *testing.T) {
+	assert.Equal(t, 6, CalculateYesCount([]string{
+		"abcx",
+		"abcy",
+		"abcz",
+	}))
+}
+
+func TestWithMultipleGroups(t *testing.T) {
+	assert.Equal(t, 11, CalculateYesCount([]string{
+		"abc",
+		"",
+		"a",
+		"b",
+		"c",
+		"",
+		"ab",
+		"ac",
+		"",
+		"a",
+		"a",
+		"a",
+		"a",
+		"",
+		"b",
+	}))
+}
+
+func TestWithMultipleGroups2(t *testing.T) {
+	assert.Equal(t, 23, CalculateYesCount([]string{
+		"c",
+		"omp",
+		"yk",
+		"w",
+		"",
+		"u",
+		"fq",
+		"",
+		"qkvm",
+		"qvlmk",
+		"vmq",
+		"bvmrq",
+		"ifmqxagvu",
+	}))
+}
